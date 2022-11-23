0faf4edc82baabfb7ec0e03c3154d9879b6230b8
JohanPer <johan.perrusson@hotmail.fr>
2022-11-23 12:08:36 +0100

    before action profile gucci

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, only: [:profile]
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || root_path(anchor: 'redirect_logged')
   end
