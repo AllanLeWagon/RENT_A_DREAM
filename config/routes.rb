9c486dab6ad3f46c925bb1f0f43a5d86b9148ffb
JohanPer <johan.perrusson@hotmail.fr>
2022-11-23 11:57:23 +0100

    added profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'profile', to: 'users#profile'
   get 'articles/new'
   get 'articles/index'
   get 'articles/create'
   devise_for :users
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
