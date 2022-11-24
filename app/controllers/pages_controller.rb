56442fd81a43e3b26f4c034a082999dc95ebb006
Ines Lagarde <ineslagardealcon@gmail.com>
2022-11-24 14:04:31 +0100

    Added search bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    @articles = Article.all
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR service_type ILIKE :query OR description ILIKE :query"
+      @articles = Article.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @articles = Article.all
+    end
   end
 end
